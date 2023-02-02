906a083a18a37619a43355ebf1c475186318ed9c
Victoria Flama <116015637+vickiflama@users.noreply.github.com>
2023-02-02 08:29:07 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+/*!
+* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+});
+
+function inclusivo () {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color= "white";
+    document.body.style.h1="white";
+    document.getElementById("nombre").style.color="white";
+    document.getElementById("experiencia").style.color="white";
+    document.getElementById("educacion").style.color="white";
+    document.getElementById("habilidades").style.color="white";
+    document.getElementById("intereses").style.color="white";
+}
+
